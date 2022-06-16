deb60bdfdf892efcb1ecc3b5c92d9d503bbe4d8c
PalakGJ <107097101+PalakGJ@users.noreply.github.com>
2022-06-16 14:36:29 +0530

    Add files via upload

---
+++ ConsoleApp1/PageObjectModelTodos.cs
@@ -0,0 +1,123 @@
+﻿/* ***************************************************************
+* 
+* Author: Palak Joshi
+* Date: June 09, 2022
+* Changes
+* Date Description Author
+* <09-June-2021> <Initial file creation> <Palak Joshi>
+*****************************************************************/
+
+
+using OpenQA.Selenium;
+using OpenQA.Selenium.Support.UI;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+
+namespace Enablon.ProductModel
+{
+    public class Todolist
+    {
+        #region Methods
+
+        /// <summary>
+        /// Add record in the grid
+        /// /// </summary> 
+        /// <param name="driver">Report name to be search</param>
+        public bool AddRecord(WebDriver driver)
+        {
+          
+            IWebElement record = driver.FindElement(By.XPath("//input[@placeholder='What needs to be done?']"));
+           
+            record.SendKeys("Palak111");
+            record.SendKeys(Keys.Enter);
+
+            record.SendKeys("Palak112");
+            record.SendKeys(Keys.Enter);
+
+            record.SendKeys("Palak");
+            record.SendKeys(Keys.Enter);
+        
+            return true;
+        }
+        /// <summary>
+        /// Select or check out the record from the grid
+        /// /// </summary> 
+        /// <param name="driver">Report name to be search</param>
+        public bool CheckOutRecordFromGrid(WebDriver driver)
+        {
+            IWebElement selectRecordCheckbox = driver.FindElement(By.XPath("//input[@class='toggle']"));
+            selectRecordCheckbox.Click();
+            VerifyClearCompletedButtonIsEnabled(driver ,selectRecordCheckbox);
+            return true;
+        }
+
+        /// <summary>
+        /// Remove record from grid by clicking X button on the right side of button. 
+        /// /// </summary> 
+        /// <param name="driver">Report name to be search</param>
+        public bool RemoveRecordByClickingXButton(WebDriver driver)
+        {
+            IWebElement xButton = driver.FindElement(By.XPath("//button[@class='destroy']"));   //button[@class='destroy']
+            xButton.Click();
+            return true;
+        }
+        /// <summary>
+        /// Verify Clear completed button is enabled once any record is checkedout or selected
+        /// /// </summary> 
+        /// <param name="driver, selectrechordCheckbox">Report name to be search</param>
+        public void VerifyClearCompletedButtonIsEnabled(WebDriver driver, IWebElement selectRecordCheckbox)
+        {
+            if (selectRecordCheckbox.Selected)
+            {
+                IWebElement btnClearCompleted = driver.FindElement(By.XPath("//button[text() = 'Clear completed']"));
+                Console.WriteLine("Clear button enabled : {0} ", btnClearCompleted.Enabled ? "T" : "F");
+                if (btnClearCompleted.Enabled)
+                {
+                    int count = 0;
+                    count++;
+                }
+                else
+                {
+                }
+
+            }
+        }
+
+        /// <summary>
+        /// Search record which is deleted still exist in the list 
+        /// /// </summary> 
+        /// <param name="Recordname">Report name to be search</param>
+        public bool VerifyDeletedRecordDoesNotDisplayInTheGrid(WebDriver record)
+            {                  
+
+            List<IWebElement> elementList = new List<IWebElement>();
+            elementList.AddRange(record.FindElements(By.XPath("//label[text()='Palak112']")));
+            int cnt = elementList.Count;
+            RemoveRecordByClickingXButton(record);
+            try
+            {
+                bool isElementDisplayed = record.FindElement(By.XPath("//label[text()='Palak112']")).Displayed;
+            }
+            catch (NoSuchElementException ex)
+            {
+                Console.WriteLine("No record exist...Record is deleted");
+            }
+                        
+            return true;
+        }
+                     
+
+     }
+             #endregion
+
+        public class RecordVO
+        {
+            public string Recordname { get; set; }
+
+        }
+    
+       
+    
+}
---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,88 @@
+﻿using System;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using OpenQA.Selenium.Support.UI;
+using Enablon.ProductModel;
+using log4net.Config;
+using log4net;
+using System.Collections.Generic;
+
+namespace ConsoleApp1
+{
+
+    public class Program : Todolist
+    {
+        
+        private static IWebElement selectRecordCheckbox;   
+                
+        public static void Main()
+        {
+            WebDriver driver = new ChromeDriver();
+          
+            driver.Navigate().GoToUrl("https://todomvc.com/examples/angular2/");
+            driver.Manage().Window.Maximize();
+            System.Threading.Thread.Sleep(5000);
+
+
+            //Configure Log4net
+            BasicConfigurator.Configure();
+            //Create a logger
+            ILog logger = LogManager.GetLogger(typeof(Program));
+            //Writer logs in our test
+
+            System.Threading.Thread.Sleep(5000);
+
+            Testcase01_AddRecordTodoList(driver);
+            logger.Debug("Records are added in the Grid");
+
+            Testcase02_CheckOutRecordFromTodoList(driver);
+            logger.Debug("On Selecting record the record gets checkedout in the Grid");
+            selectRecordCheckbox = driver.FindElement(By.XPath("//input[@class='toggle']"));
+
+            Testcase03_VerifyClearCompletedButtonIsEnabled(driver, selectRecordCheckbox);
+            logger.Debug("on selecting any record the clearcompletedbutton gets enabled");
+
+            Testcase04_RemoveRecordByClickingCrossButton(driver);
+            logger.Debug("On Clicking X button on the record hove or select , the record gets removed");
+
+            Testcase05_VerifyTheRemovedDataDoesNotExistInGridAnyMore(driver);
+            logger.Debug("Once the data is removed, verify that same data still exist");
+
+        }
+
+        public static void Testcase01_AddRecordTodoList(WebDriver driver)
+        {
+            Todolist todoList1 = new Todolist();
+            todoList1.AddRecord(driver);
+           
+        }
+        public static void Testcase02_CheckOutRecordFromTodoList(WebDriver driver)
+        {
+            Todolist todoList2 = new Todolist();
+            todoList2.CheckOutRecordFromGrid(driver);
+        }
+
+        public static void Testcase03_VerifyClearCompletedButtonIsEnabled(WebDriver driver, IWebElement selectRecordCheckbox)
+        {
+            Todolist todoList3 = new Todolist();
+            todoList3.VerifyClearCompletedButtonIsEnabled(driver, selectRecordCheckbox);
+                
+        }
+
+        public static void Testcase04_RemoveRecordByClickingCrossButton(WebDriver driver)
+        {    
+            Todolist todoList3 = new Todolist();
+            todoList3.RemoveRecordByClickingXButton(driver);
+        }
+
+        public static void Testcase05_VerifyTheRemovedDataDoesNotExistInGridAnyMore(WebDriver record)
+        {
+            Todolist todoList3 = new Todolist();
+            todoList3.VerifyDeletedRecordDoesNotDisplayInTheGrid(record);
+
+        }
+        
+        
+
+    }
+}
